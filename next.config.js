996e4e9eb427f3e318d60fb37cfa78566cc33447
fuuki1993 <koreaquarter@gmail.com>
2024-07-17 12:56:54 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // basePath: '/iphone-flashcard-app',
-  // assetPrefix: '/iphone-flashcard-app/',
-  // output: 'export',
-};
-
-module.exports = nextConfig;
+module.exports = {
+  basePath: process.env.NODE_ENV === 'production' ? '/your-repo-name' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/your-repo-name/' : '',
+  images: {
+    loader: 'akamai',
+    path: '',
+  },
+}
