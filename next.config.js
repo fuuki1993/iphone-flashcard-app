d52581f081b78b30d683da13812630cf9f18e98b
fuuki1993 <koreaquarter@gmail.com>
2024-07-20 05:07:15 +0900

    変更を適用

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 const nextConfig = {
   output: 'export',
   basePath: process.env.NODE_ENV === 'production' ? '/iphone-flashcard-app' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/iphone-flashcard-app' : '',
   images: {
     unoptimized: true,
   },
