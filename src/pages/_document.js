19d5f34d72afff4a780459761d92ff707e57d3e6
fuuki1993 <koreaquarter@gmail.com>
2024-07-20 02:12:44 +0900

    _

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -2,9 +2,14 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html>
+    <Html lang="en">
       <Head>
         <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/icon-192x192.png" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="iPhone Flashcard App" />
+        <meta name="theme-color" content="#000000" />
       </Head>
       <body>
         <Main />
